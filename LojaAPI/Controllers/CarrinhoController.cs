7331b7ae143749a9829b7d95550d579a32ea93f8
notgnihsaw-pc <washy17@gmail.com>
2017-02-24 07:25:55 -0300

    fistr commit

---
+++ LojaAPI/Controllers/CarrinhoController.cs
@@ -0,0 +1,29 @@
+﻿using Loja.DAO;
+using Loja.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace LojaAPI.Controllers
+{
+    public class CarrinhoController : ApiController
+    {
+        public Carrinho Get(int id)
+        {
+            CarrinhoDAO dao = new CarrinhoDAO();
+            Carrinho carrinho = dao.Busca(id);
+            return carrinho;
+        }
+
+        public string Post([FromBody] Carrinho carrinho)
+        {
+            CarrinhoDAO dao = new CarrinhoDAO();
+            dao.Adiciona(carrinho);
+            return "Sucesso";
+        }
+
+    }
+}
